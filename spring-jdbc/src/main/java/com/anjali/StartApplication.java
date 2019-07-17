6be6487d179697ab5115fbf5bd383dfd2934da04
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-16 19:51:45 -0600

    Update StartApplication.java

--- spring-jdbc/src/main/java/com/anjali/StartApplication.java
+++ spring-jdbc/src/main/java/com/anjali/StartApplication.java
@@ -1,6 +1,6 @@
-package com.mkyong;
+package com.anjali;
 
-import com.mkyong.repository.BookRepository;
+import com.anjali.repository.BookRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
