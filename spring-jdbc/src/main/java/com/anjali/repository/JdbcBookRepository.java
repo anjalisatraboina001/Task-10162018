1ad535caaadf83f7452d01ab151ebcd9963cb0a9
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-16 19:50:31 -0600

    Update JdbcBookRepository.java

--- spring-jdbc/src/main/java/com/anjali/repository/JdbcBookRepository.java
+++ spring-jdbc/src/main/java/com/anjali/repository/JdbcBookRepository.java
@@ -1,6 +1,6 @@
-package com.mkyong.repository;
+package com.anjali.repository;
 
-import com.mkyong.Book;
+import com.anjali.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
